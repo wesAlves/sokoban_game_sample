e2bc3a3c4a5415c4861d126f2eacfd83345fd0d8
Wes Benvindo <wal2.designer@gmail.com>
2025-03-18 07:29:53 +0000

    starting

---
+++ Game.py
@@ -0,0 +1,11 @@
+import pygame
+
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.window = pygame.display.set_mode(size=(640, 640))
+
+    def run(self):
+        while True:
+            pass
---
+++ main.py
@@ -0,0 +1,6 @@
+from Game import Game
+
+game = Game()
+
+if __name__ == "__main__":
+    game.run()
